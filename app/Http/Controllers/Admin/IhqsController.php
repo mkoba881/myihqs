8cf33d44dca5ad2ac587431f933ede80d377ef92
masato <mkoba881@gmail.com>
2023-06-02 03:59:03 +0000

    アンケート管理画面修正

--- app/Http/Controllers/Admin/IhqsController.php
+++ app/Http/Controllers/Admin/IhqsController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+// 以下の1行を追記することで、Qn Modelが扱えるようになる
+use App\Models\Qn;
+
 class IhqsController extends Controller
 {
     public function add()
@@ -39,9 +42,18 @@ class IhqsController extends Controller
 
     public function makepreview()
     {
+        $this->validate($request, Qn::$rules);
+        $qn = new Qn;
+        $form = $request->all();
+        // フォームから送信されてきた_tokenを削除する
+        unset($form['_token']);
+        // データベースに保存する
+        $qn->fill($form);
+        $qn->save();
+     
         return view('fs.makepreview');
     }
-    public function deleteqn()
+    public function deleteqn()  
     {
         return view('fs.deleteqn');
     }
