5dae0ac62e3684b09b2b06c5d1b099c20d2847a3
masato <mkoba881@gmail.com>
2023-09-24 07:59:04 +0900

    重複のバリデーションルールの作成

--- app/Http/Requests/AnkateCreateRequest.php
+++ app/Http/Requests/AnkateCreateRequest.php
@@ -30,16 +30,51 @@ class AnkateCreateRequest extends FormRequest
 
         $questionCount = $this->input('questionCount');
 
+        // for ($i = 1; $i <= $questionCount; $i++) {
+            
+            
+        //     $rules["question_name{$i}"] = 'required|string';
+            
+        //     $rules["question{$i}"] = 'required|string';
+        //     $rules["sortorder{$i}"] = 'required|string';
+        //     // 他の質問に関するルールも追加
+        // }
+        
+        $existingQuestionNames = [];
+        $existingQuestions = [];
+        $existingSortOrders = [];
+        
         for ($i = 1; $i <= $questionCount; $i++) {
+            $questionNameKey = $this->input("question_name{$i}");
+            $questionKey = $this->input("question{$i}");
+            $sortOrderKey = $this->input("sortorder{$i}");
+        
+            // 既存のフィールド名と重複するかチェック
+            if (in_array($questionNameKey, $existingQuestionNames)) {
+                die("質問名が重複しています。");
+            }
             
-            
+            if (in_array($questionKey, $existingQuestions)) {
+                die("質問が重複しています。");
+            }
+        
+            if (in_array($sortOrderKey, $existingSortOrders)) {
+                die("ソート順が重複しています。");
+            }
+        
+            // フィールド名を追加
+            $existingQuestionNames[] = $questionNameKey;
+            $existingQuestions[] = $questionKey;
+            $existingSortOrders[] = $sortOrderKey;
+        
+            // バリデーションルールを設定
             $rules["question_name{$i}"] = 'required|string';
-            
             $rules["question{$i}"] = 'required|string';
             $rules["sortorder{$i}"] = 'required|string';
+        
             // 他の質問に関するルールも追加
         }
-
+        
         return $rules;
     }
 
